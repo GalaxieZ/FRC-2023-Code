8e78b64411a3e4b05fb6e2befd5d154376d71e3a
ManishReddy10 <122648051+ManishReddy10@users.noreply.github.com>
2023-03-18 08:30:36 -0700

    Claw subsystem added

---
+++ src/main/java/frc/robot/commands/ActivateSolenoids.java
@@ -0,0 +1,49 @@
+
+
+
+//NEED TO FIGURE OUT HOW TO IMPLEMENT EXISTING METHODS FROM SUBSYSTEM CLAW INTO ROBOT CONTAINER OR MAKE THEM INTO COMMANDS 
+
+
+
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
+
+// package frc.robot.commands;
+
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+
+// import frc.robot.subsystems.Claw;
+
+// public class ActivateSolenoids extends CommandBase {
+//   /** Creates a new ActivateSolenoids. */
+//   private final Claw;
+//   boolean bool = true;
+//   public ActivateSolenoids(Claw claw) {
+//     // Use addRequirements() here to declare subsystem dependencies.
+//     bool = true;
+//   }
+
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {}
+
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
+//     bool = !bool;
+//     if (bool = true){
+//        DoubleSolenoidR.set(forward);
+//     }
+//   }
+
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {}
+
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
